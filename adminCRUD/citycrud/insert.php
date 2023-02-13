7502ac28193b0befa2ee383234c66dc8fe205c27
Harrismich <117765164+Harrismich@users.noreply.github.com>
2023-02-13 08:59:45 +0200

    admin

--- adminCRUD/citycrud/insert.php
+++ adminCRUD/citycrud/insert.php
@@ -8,19 +8,64 @@ values ('$city_name', '$description')");
 $res = $dbc->query("select city_id from city order by city_id desc");
 $row = $res->fetch_row();
 $city_id = $row[0];
- 
-// Set a constant for the file repository
-define("FILEREPOSITORY", "http://localhost/project%20php/bootstrap-travel-website-templates/images/");
 
+
+if (isset($_POST['submit']) && isset($_FILES['my_image'])) {
+	include "db_conn.php";
+
+	echo "<pre>";
+	print_r($_FILES['my_image']);
+	echo "</pre>";
+
+	$img_name = $_FILES['my_image']['name'];
+	$img_size = $_FILES['my_image']['size'];
+	$tmp_name = $_FILES['my_image']['tmp_name'];
+	$error = $_FILES['my_image']['error'];
+
+	if ($error === 0) {
+		if ($img_size > 125000) {
+			$em = "Sorry, your file is too large.";
+		    header("Location: city.php?error=$em");
+		}else {
+			$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+			$img_ex_lc = strtolower($img_ex);
+
+			$allowed_exs = array("jpg", "jpeg", "png"); 
+
+			if (in_array($img_ex_lc, $allowed_exs)) {
+				$new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+				$img_upload_path = 'uploads/'.$new_img_name;
+				move_uploaded_file($tmp_name, $img_upload_path);
+
+				// Insert into Database
+				$sql = "INSERT INTO images(image_url) 
+				        VALUES('$new_img_name')";
+				mysqli_query($conn, $sql);
+				header("Location: view.php");
+			}else {
+				$em = "You can't upload files of this type";
+		        header("Location: index.php?error=$em");
+			}
+		}
+	}else {
+		$em = "unknown error occurred!";
+		header("Location: index.php?error=$em");
+	}
+
+}else {
+	header("Location: index.php");
+}
 // Make sure that the file was POSTed
 if (isset($_POST["submit"])) {
-  $target_dir = "../images/";
+  $target_dir = "C:\\xampp\\htdocs\\project php\\bootstrap-travel-website-templates\\images\\";
   $target_file = $target_dir . $city_name . $city_id . ".jpg";
+  $photo_name = "" . $city_name . $city_id . "";
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
   // Check if image file is a actual image or fake image
-  $check = getimagesize($_FILES["pimage"]["tmp_name"]);
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  echo'$check';
   if ($check !== false) {
     $uploadOk = 1;
   } else {
@@ -35,7 +80,7 @@ if (isset($_POST["submit"])) {
   }
 
   // Check file size
-  if ($_FILES["pimage"]["size"] > 500000) {
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
   }
@@ -51,9 +96,9 @@ if (isset($_POST["submit"])) {
     echo "Sorry, your file was not uploaded.";
   // if everything is ok, try to upload file
   } else {
-    if (move_uploaded_file($_FILES["pimage"]["tmp_name"], $target_file)) {
-      echo "The file " . htmlspecialchars(basename($_FILES["pimage"]["name"])) . " has been uploaded.";
-      $dbc->query("insert into image (city_id, path) values ('$city_id', '$target_file')");
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+      $dbc->query("insert into image (city_id, path) values ('$city_id', '$photo_name')");
     } else {
       echo "Sorry, there was an error uploading your file.";
     }
